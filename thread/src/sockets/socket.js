94b277d760cd5776385c720e57629c01aa9f2d2c
Khoa <khoat0360@gmail.com>
2025-07-21 17:32:46 +0700

    update domainbackend

--- thread/src/sockets/socket.js
+++ thread/src/sockets/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:5000";
+const URL = "http://localhost:5000" || "https://clone-instagram-117m.onrender.com";
 
 // Add a 'type' parameter to distinguish connections
 export const createSocket = (userId, type = 'main') => {
